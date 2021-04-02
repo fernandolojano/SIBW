4781ed5a2ff5aadd1d3232f8483cdbf11c06bfd8
GFernando97 <e.fernandolojano@go.ugr.es>
2021-04-02 11:39:11 +0200

    Almost...

--- Practica 2/Web/code.js
+++ Practica 2/Web/code.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+
+function openPanel() {
+	document.getElementById("myCommentsPanel").style.height = "100%";
+	document.getElementById("myCommentsPanel").style.opacity = "1";	
+	document.getElementById("buttonOpen").style.zIndex="1";
+
+}
+
+
+function closePanel(){
+	document.getElementById("myCommentsPanel").style.height = "0";
+	document.getElementById("myCommentsPanel").style.opacity= "0";
+	document.getElementById("buttonOpen").style.zIndex="6";
+}
+
+window.onload = function(){
+document.getElementById("buttonOpen").onclick = openPanel;
+document.getElementById("buttonClose").onclick = closePanel;
+	
+}
+
+
+
